666697817a3a2886f75d47d545e5f1dad93474d2
MarceloRLima <116229332+MarceloRLima@users.noreply.github.com>
2022-10-20 01:20:55 +0000

    Como exibir os strings de maneiras diferente

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ idade = 21
 #Exibir o meu nome (que está dentro da variável nome)
 print(nome)
 
-  
+  # Quando quiser exibir a frase "Minha idade é " completando com o conteúdo variável idade
+#print("Meu nome é "+nome)
+print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("Minha idade é {}".format(idade))
+
+#Quando quiser exibir "Meu nome é... e tenho... anos" trocando pelas viariáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
+
+
 
 
 '''
