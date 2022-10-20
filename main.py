17e62fe7c37e714c79319259d97947b2576f99d4
MarceloRLima <116229332+MarceloRLima@users.noreply.github.com>
2022-10-20 00:53:17 +0000

    Aprendi a criar variáveis e exibi-las na tela!

--- main.py
+++ main.py
@@ -3,6 +3,15 @@
 # print() = comando de saida
 print("Alô mundo!")
 
+# Quando quiser guardar uma String! (frase)   
+nome = "Marcelo Lima"
+# Quando quiser guardar um número inteiro
+idade = 21
+
+#Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+  
 
 
 '''
