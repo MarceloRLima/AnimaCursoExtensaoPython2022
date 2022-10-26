4f868fac9676e07603d4c6e800fa4b59da506098
MarceloRLima <116229332+MarceloRLima@users.noreply.github.com>
2022-10-26 23:14:33 +0000

    Criação de outra pasta; Uso do comando imput e a diferença entra uma variável int e sring

---
+++ aula1-2022-10-19.py
@@ -0,0 +1,29 @@
+# Meu primeiro projeto Python!!!
+#
+# print() = comando de saida
+print("Alô mundo!")
+
+# Quando quiser guardar uma String! (frase)   
+nome = "Marcelo Lima"
+# Quando quiser guardar um número inteiro
+idade = 21
+
+#Exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+  # Quando quiser exibir a frase "Minha idade é " completando com o conteúdo variável idade
+#print("Meu nome é "+nome)
+print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("Minha idade é {}".format(idade))
+
+#Quando quiser exibir "Meu nome é... e tenho... anos" trocando pelas viariáveis nome e idade
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
+
+
+
+
+'''
+Comentario em bloco
+
+'''
--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-# Meu primeiro projeto Python!!!
-#
-# print() = comando de saida
-print("Alô mundo!")
+# comando imput(): Quero permitir que o usuário digite algo...
+nome=input("Digite seu nome: ")
 
-# Quando quiser guardar uma String! (frase)   
-nome = "Marcelo Lima"
-# Quando quiser guardar um número inteiro
-idade = 21
+#pede a idade para o usuário "Qual sua idade?
+idade=int(input("Qual a sua idade? "))
 
-#Exibir o meu nome (que está dentro da variável nome)
-print(nome)
+# comando de saída... exibir na tela
+print(f"\nBoa noite, seu nome é {nome}")
+#exiba "Sua idade é..."
+print(f"Sua idade é {idade} anos")
 
-  # Quando quiser exibir a frase "Minha idade é " completando com o conteúdo variável idade
-#print("Meu nome é "+nome)
-print("Minha idade é "+str(idade)+" anos")
-print(f"Minha idade é {idade} anos")
-print("Minha idade é {}".format(idade))
-
-#Quando quiser exibir "Meu nome é... e tenho... anos" trocando pelas viariáveis nome e idade
-print("Meu nome é {} e tenho {} anos".format(nome, idade))
-
-
-
-
-'''
-Comentario em bloco
-
-'''
+#e se eu quisesse mostrar o DOBRO da idade informada
+dobro = idade * 2
+print("O dobro da idade informada é {}".format(dobro))
